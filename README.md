e727755ee33a2f4d60fe67e458327771cb21c36a
DanielWTE <daniel.iuser@icloud.com>
2023-06-18 16:31:25 +0200

    Initial

--- README.md
+++
@@ -1,2 +0,0 @@
-# solun-database-package
-This package is used to manage the database tools and models of solun.
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// src: src/utils/database/dbConn.ts
+import dbConnect from './utils/database/dbConn';
+export { dbConnect };
+
+// src: src/utils/database/dbUtils.ts
+import { findOneDocument, deleteOneDocument } from './utils/database/dbUtils';
+export { findOneDocument, deleteOneDocument };
+
+// src: src/models
+import AppPassword from './models/appPasswords';
+import File from './models/file';
+import Message from './models/message';
+import TempToken from './models/tempToken';
+import User from './models/user';
+import FunctionErrLog from './models/functionErrLog';
+export { AppPassword, File, Message, TempToken, User, FunctionErrLog };
+
+// src: src/bird/handler
+import { saveError } from './bird/handler';
+export { saveError };
