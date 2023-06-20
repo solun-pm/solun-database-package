62ba3f40870d01baf5951718267e148b9be6a917
DanielWTE <daniel.iuser@icloud.com>
2023-06-20 20:19:33 +0200

    Updated handler with error exception

--- src/bird/handler.ts
+++ src/bird/handler.ts
@@ -2,27 +2,35 @@ import Log from '../models/log';
 import ApiLog from '../models/api_logs';
 
 export function birdLog(name: string, err: any, type: string) {
-    const validTypes = ['error', 'warning', 'info'];
-    if (!validTypes.includes(type)) {
-        throw new Error('Invalid type');
+    try {
+        const validTypes = ['error', 'warning', 'info'];
+        if (!validTypes.includes(type)) {
+            throw new Error('Invalid type');
+        }
+        const log = new Log({
+            log_name: name,
+            log_message: err,
+            type: type,
+        });
+        log.save();
+    } catch (err) {
+        console.error(err);
     }
-    const log = new Log({
-        log_name: name,
-        log_message: err,
-        type: type,
-    });
-    log.save();
 }
 
 export function birdApiLog(method: string, status: number, url: string, ms: number, remoteAddress: string, referrer: string, userAgent: string) {
-    const apiLog = new ApiLog({
-        method: method,
-        status: status,
-        url: url,
-        ms: ms.toFixed(0),
-        remoteAddress: remoteAddress,
-        referrer: referrer,
-        userAgent: userAgent,
-    });
-    apiLog.save();
+    try {
+        const apiLog = new ApiLog({
+            method: method,
+            status: status,
+            url: url,
+            ms: ms.toFixed(0),
+            remoteAddress: remoteAddress,
+            referrer: referrer,
+            userAgent: userAgent,
+        });
+        apiLog.save();
+    } catch (err) {
+        console.error(err);
+    }
 }
