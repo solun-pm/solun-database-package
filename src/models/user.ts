0b618cea5cdd98b944de5642c1d0d6a05cec3fb9
DanielWTE <daniel.iuser@icloud.com>
2023-07-01 12:52:08 +0200

    updated user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -69,6 +69,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    recovery_key: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
