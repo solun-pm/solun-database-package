b8b8f566e1a93fcb27838373634bbd6d8adf541a
DanielWTE <daniel.iuser@icloud.com>
2023-07-02 16:01:48 +0200

    added active to aliases

--- src/models/user_aliases.ts
+++ src/models/user_aliases.ts
@@ -23,6 +23,11 @@ const user_aliases = new mongoose.Schema(
         type: String,
         required: true,
     },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
   },
   {
     timestamps: true,
